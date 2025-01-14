a599453b9c820797630b0cdee37d0e273c2fcf00
helenh <hhgebremichael@gmail.com>
2025-01-14 21:44:11 +0100

    add

--- Session_1.py
+++ Session_1.py
@@ -1,1 +1,12 @@
-print("Hello")
+##print("Hello")
+# print (" Hello world")
+##name = "Sam" # string -- Anything wrapped in double or single quote.
+##price_psq = "123"  #  
+#greeting = " Hello!"
+#greeting = " Hello World"
+#print (greeting)
+# Send a hello to the world !
+#calculation = 5 ** 3
+#print (calculation)
+a = "40"
+print (type(a))
